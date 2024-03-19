4195b681a74be98e200e4d2e9dc90d80d900ec42
hashpoy <hashpoy4132@protonmail.com>
2024-03-19 21:07:54 +0800

    update assets and work

---
+++ assets/js/page--src--pages--resume--index-vue.d2b2c29a.js
@@ -0,0 +1,356 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{238:function(e,t,n){},244:function(e,t,n){"use strict";var i=n(1),a=Object(i.a)({},(function(e,t){var n=t._c;return n("svg",t._g(t._b({},"svg",Object.assign({height:"24",viewBox:"0 59.88199996948242 479.0579833984375 359.29400634765625",width:"24"},t.data.attrs),!1),t.data.on),[n("path",{attrs:{d:"M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912\n    44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0\n    29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 015.738-1.159zm0\n    299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655\n    9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"}})])}),[],!0,null,null,null).exports,o=Object(i.a)({},(function(e,t){var n=t._c;return n("svg",t._g(t._b({},"svg",Object.assign({role:"img",viewBox:"0 0 24 24",width:"24",height:"24"},t.data.attrs),!1),t.data.on),[n("path",{attrs:{d:"M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574\n    2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92\n    2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666\n    1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693\n    4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604\n    3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557\n    2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56\n    2.46-2.548l-.047-.02z"}})])}),[],!0,null,null,null).exports,r=Object(i.a)({},(function(e,t){var n=t._c;return n("svg",t._g(t._b({},"svg",Object.assign({role:"img",viewBox:"0 0 24 24",width:"24",height:"24"},t.data.attrs),!1),t.data.on),[n("path",{attrs:{d:"M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882\n    4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156\n    4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388\n    7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103\n    22.9522 23.9981 17.9882 23.9981 11.9991Z"}})])}),[],!0,null,null,null).exports,s=Object(i.a)({},(function(e,t){var n=t._c;return n("svg",t._g(t._b({},"svg",Object.assign({role:"img",viewBox:"0 0 24 24",width:"24",height:"24"},t.data.attrs),!1),t.data.on),[n("path",{attrs:{d:"M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136\n    2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267\n    5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0\n    2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225\n    0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24\n    22.271V1.729C24 .774 23.2 0 22.222 0h.003z"}})])}),[],!0,null,null,null).exports,l=Object(i.a)({},(function(e,t){var n=t._c;return n("svg",t._g(t._b({},"svg",Object.assign({height:"24",viewBox:"8 8.003 48 47.997",width:"24"},t.data.attrs),!1),t.data.on),[n("path",{attrs:{d:"M29.283 12.19a19.624 19.624 0 015.434 0 2 2 0 00.567-3.959 23.75 23.75 0 00-6.566 0 2 2 0 00.565 3.959z"}}),n("path",{attrs:{d:"M42.91 10.619a2 2 0 00-1.82 3.561C47.819 17.621 52 24.449 52 32c0 11.028-8.972 20-20\n    20s-20-8.972-20-20c0-7.551 4.181-14.379 10.911-17.819a2 2 0 00-1.821-3.561C13.016 14.747 8 22.939 8\n    32c0 13.233 10.766 24 24 24 13.233 0 24-10.767 24-24 0-9.061-5.016-17.253-13.09-21.381z"}}),n("path",{attrs:{d:"M22.586 36.586a2 2 0 000 2.828l8 8c.39.391.902.586 1.414.586s1.023-.195 1.414-.586l8-8a2 2 0\n    10-2.828-2.828L34 41.172V18a2 2 0 00-4 0v23.172l-4.586-4.586a2 2 0 00-2.828 0z"}})])}),[],!0,null,null,null).exports;n.d(t,"d",(function(){return a})),n.d(t,"e",(function(){return o})),n.d(t,"b",(function(){return r})),n.d(t,"c",(function(){return s})),n.d(t,"a",(function(){return l}))},247:function(e,t,n){"use strict";var i=n(238);n.n(i).a},257:function(e,t,n){"use strict";n.r(t);var i={companies:[{name:"Shopify",period:"May 2020 - Present",role:"Senior Frontend Engineer",intro:"Shaping the future of commerce and entrepreneurship – building and advocating for inclusive and thoughtful experiences for over a million merchants across the globe.",points:["Pioneering initiatives around the future of onboarding, signup and developing technical vision for them","Collaborating with UX designers and researchers to inform the direction of products we craft","First-hand involvement in early-stage product and UX thinking and rapid prototyping","Conducting and monitoring several strategic experiments, refining experiences based on resulting metrics and learnings","Participating and incorporating learnings from real merchant interviews and research sessions","Identifying gaps around the org-wide frontend infrastructure, drafting proposals and implementing solutions","Mentoring engineers within and outside my team on best practices, performance and advanced patterns","Providing guidance around product accessibility and inclusivity across the org","Built an OAuth 2.0 compliant client-side library for our Identity provider across Shopify"]},{name:"hellotax",period:"Mar - Nov 2019",role:"Principal Frontend Engineer (Contractor)",intro:"I orchestrated and developed the frontend infrastructure, design and functionality for hellotax's new JavaScript-centric app powered by Vue and also managed a co-engineer.",points:["Crafted a scalable component library that enforced accessibility and fluency across the application.","Built a multi-role user authentication system from the ground up with JavaScript.","Implemented a client-side localization system for the entire app.","Built a client-side multilingual routing system with solid navigation guards.","Engineered fail-safe optimized utility libraries to minimize runtime errors and unusability in production.","Implemented fast and reliable data transformation and diffing mechanisms on the frontend for our data pipelines.","Delivered an overall fast, yet accessible and silky smooth user experience in the app."]},{name:"Pixel2HTML",period:"Nov 2018 - Oct 2019",role:"Senior Frontend Engineer (Contractor)",intro:"I worked on a couple of projects for organizations and individuals as a frontend engineering contractor.",points:["Built frontend applications for multiple clients. Most especially JavaScript and React applications.","Rebuilt the company's marketing site initially written in HTML/CSS using React and Gatsby.","Crafted, audited and improved design, accessibility and overall experience of products.","Enforced healthy codebases, coding style and standards.","Completed projects on time and delivered to clients accordingly.","Communicated effectively with project managers and the team."]},{name:"MOONDA",period:"Nov 2018 - Jan 2019",role:"Senior Frontend Engineer (Contractor)",intro:"I worked with MOONDA as a senior contractor via Pixel2HTML to create the web experience for John Deere's ATU 300 product.",points:["Built a contest application from the ground up using React which enabled users of the product from all around the globe to post photos and share reviews about the product.","Designed and implemented a cross-browser, IE 11 compatible marketing site for the product.","Implemented internationalization on the website with support for over 40 languages.","Implemented automatic locale detection (with proper redirection based on that) and locale-specific content filtering."]},{name:"Conectar",period:"Jun - Nov 2018",role:"Lead Frontend Engineer",intro:"I led a team of developers through building multiple React applications into a single powerful online learning platform. I also re-engineered and designed several components on the platform.",points:["Crafted a solid design system to enforce consistency across the application.","Built a messaging system as powerful as Facebook Messenger with React.","Built a multi-feature audio and video call application powered by React.","Introduced and enforced a consistent coding style across the application.","Eliminated dead code which saved us about 20% the size of every page bundle.","Eliminated Bootstrap and other CSS libraries completely from the application, and replaced them with custom styles.","Reduced bundle size from 5MB to a few kilobytes.","Did a major re-engineering of the development workflow which led to over 70% increase in development compile speed.","Built an appointment scheduling system from the ground up with React."],outro:'See the extended list <a target="_blank" rel="noreferrer noopener" href="https://docs.google.com/document/d/1Gw4mcURKddzOJ7OoZsMghJDAhW5zMXler4EeOY9BP6E/edit">here.</a>'},{name:"Pneuma Media",period:"Dec 2013 - Feb 2018",role:"Full Stack Developer",intro:"I worked as the only engineer with this company. Built and maintained the backend and frontend of a couple of web apps for clients.",points:[]}],technologies:["JavaScript","React","CSS-in-JS","TypeScript","Vue.js","Sass","Flux","ESNext","Flow","NodeJS","GraphQL","LessCSS","Next.js"],otherProficiencies:["Design Systems","E2E Testing","PWAs","Performance Optimization","Software Testing","SE Optimization","Progressive Enhancement","Accessibility auditing","Build Automation","Responsive Web Design","UX Design/Strategy"]},a=n(56),o=n(0);let r,s,l,d=e=>e;const c=Object(o.b)(r||(r=d`
+  position: relative;
+
+  #main {
+    max-width: unset;
+    display: flex;
+    padding-top: 6.7rem;
+    padding-bottom: 6.7rem;
+    position: relative;
+    background-color: var(--bg-color);
+
+    @media (max-width: 790px) {
+      flex-direction: column;
+      padding-right: var(--space);
+    }
+
+    @media (min-width: 1024px) {
+      margin-top: 0;
+    }
+
+    @media (min-width: 1200px) {
+      margin: calc(var(--space) * 1.2) 7vw;
+    }
+  }
+
+  p,
+  ul {
+    color: rgba(71, 71, 71, 0.75);
+  }
+
+  ul li {
+    position: relative;
+
+    &:before {
+      width: 0.4em;
+      content: '';
+      height: 0.4em;
+      margin-right: 0.3em;
+      vertical-align: 10%;
+      display: inline-block;
+      filter: contrast(1.2);
+      background: currentColor;
+    }
+  }
+
+  #name {
+    margin-top: 0;
+    margin-bottom: 1rem;
+    font-size: 4.5rem;
+    line-height: 1;
+    letter-spacing: -0.03em;
+    color: var(--electric-blue);
+  }
+
+  h3 {
+    margin: 0;
+  }
+
+  .meta {
+    position: absolute;
+    right: 2em;
+    top: 2em;
+    display: flex;
+    font-size: 0.9em;
+
+    a {
+      position: relative;
+      align-items: center;
+      font-weight: normal;
+      color: rgba(71, 71, 71, 0.9);
+
+      &:not([hidden]) {
+        display: flex;
+
+        @media (min-width: 791px) {
+          &[href^='mailto:'] {
+            display: none;
+          }
+        }
+      }
+
+      &:not(:last-of-type) {
+        margin-right: 1em;
+      }
+
+      &.linkedin {
+        color: #0077b5;
+        ${0}
+      }
+
+      &:last-of-type svg {
+        opacity: 0.8;
+        margin-right: 0.3em;
+      }
+
+      svg {
+        width: 1em;
+        height: 1em;
+        flex-shrink: 0;
+
+        path {
+          fill: currentColor;
+        }
+      }
+    }
+  }
+
+  #resume-outlines {
+    display: flex;
+    flex-shrink: 0;
+    position: relative;
+
+    @media (max-width: 790px) {
+      font-size: 0.9em;
+      justify-content: space-between;
+
+      ul {
+        display: flex;
+        flex-wrap: wrap;
+      }
+
+      li {
+        &:before {
+          display: none;
+        }
+
+        &:not(:last-of-type):after {
+          content: ',';
+          margin-right: 0.2em;
+        }
+      }
+    }
+
+    @media (min-width: 791px) {
+      flex-direction: column;
+    }
+
+    @media (min-width: 791px) and (max-width: 999px) {
+      margin-right: 3rem;
+    }
+
+    @media (min-width: 1000px) {
+      margin-right: 6rem;
+    }
+
+    section {
+      position: relative;
+
+      @media (max-width: 790px) {
+        &:first-of-type {
+          display: none;
+        }
+
+        &:last-of-type {
+          margin-left: 1em;
+        }
+      }
+
+      &:not(:last-of-type) {
+        margin-bottom: 2em;
+      }
+
+      h3 {
+        font-size: 1.2em;
+      }
+
+      span {
+        display: block;
+      }
+    }
+  }
+
+  article {
+    @media (min-width: 791px) {
+      max-width: 80vw;
+    }
+
+    .company > p,
+    > section > p,
+    .company .points,
+    #profile-summary > p {
+      @media (min-width: 791px) and (max-width: 999px) {
+        max-width: 90%;
+      }
+
+      @media (min-width: 1000px) {
+        max-width: 70%;
+      }
+    }
+
+    > section {
+      position: relative;
+
+      &:not(:last-of-type) {
+        margin-bottom: 5em;
+      }
+    }
+
+    p {
+      font-size: 1.2em;
+    }
+  }
+
+  #projects {
+    margin-bottom: 14em;
+
+    p {
+      font-size: 1.1em;
+
+      a {
+        margin: 0 0.2em;
+      }
+    }
+  }
+
+  #profile-summary {
+    position: relative;
+    margin-bottom: 5rem;
+
+    &:after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      width: 100%;
+      background-color: var(--border-color);
+
+      @media (min-width: 791px) {
+        margin-left: -8%;
+      }
+
+      @media (min-width: 791px) and (max-width: 1199px) {
+        width: 115%;
+      }
+
+      @media (min-width: 1200px) {
+        width: 118%;
+      }
+    }
+
+    h2 {
+      margin: 0;
+      font-weight: normal;
+    }
+
+    p {
+      margin: 3em 0 2em;
+    }
+  }
+
+  #companies {
+    position: relative;
+  }
+
+  .company {
+    position: relative;
+
+    &:before {
+      display: none;
+    }
+
+    &:not(:last-of-type) {
+      margin-bottom: 2rem;
+    }
+
+    header {
+      display: flex;
+
+      @media (max-width: 790px) {
+        flex-direction: column;
+        margin-bottom: 1em;
+
+        h4 {
+          margin-bottom: 0.2em;
+        }
+      }
+    }
+
+    .period {
+      flex-shrink: 0;
+
+      @media (min-width: 791px) {
+        margin-left: auto;
+        margin-right: 2em;
+      }
+    }
+
+    h4 {
+      margin-top: 0;
+      line-height: 1.4;
+      margin-right: 1em;
+
+      span {
+        font-weight: normal;
+        color: rgba(71, 71, 71, 0.75);
+
+        &:before {
+          content: '— ';
+        }
+      }
+    }
+
+    p {
+      margin: 0;
+    }
+
+    .outro,
+    .points {
+      font-size: 1.1em;
+    }
+
+    .points {
+      margin-top: 1em;
+      margin-left: 1em;
+
+      li {
+        text-indent: -0.97em;
+
+        &:not(:last-of-type) {
+          margin-bottom: 0.3em;
+        }
+      }
+    }
+
+    .outro {
+      margin-top: 1em;
+    }
+  }
+
+  #logo {
+    position: absolute;
+    width: 7em;
+    bottom: 2em;
+
+    @media (max-width: 790px) {
+      left: 2.5em;
+    }
+
+    @media (min-width: 791px) {
+      left: 4em;
+    }
+
+    path {
+      fill: var(--electric-blue);
+    }
+  }
+`),({isPDF:e})=>e&&Object(o.b)(s||(s=d`
+            margin-right: 0;
+          `)));Object(o.d)(l||(l=d`
+  #skip-link {
+    top: -9em;
+
+    &:focus {
+      top: -6em;
+    }
+  }
+`));var m=Object.assign({name:"StyledResume"},Object(o.c)(a.a,{isPDF:Boolean})([c])),p=(n(35),n(244)),u={name:"Résumé",data:()=>({data:i}),computed:{isPDF(){return"true"===this.$route.query.pdf},YOE:()=>(new Date).getFullYear()-2011},metaInfo:{title:"My Résumé"},components:{MailIcon:p.d,StyledResume:m,DownloadIcon:p.a,LinkedInIcon:p.c}},h=(n(247),n(1)),g=Object(h.a)(u,(function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("StyledResume",{attrs:{noBanners:"",id:"resume",isPDF:e.isPDF,title:"Olaolu's Résumé",description:"Work experience of Olaolu, expert frontend developer and UX Engineer with "+e.YOE+" years of experience"}},[n("aside",{staticClass:"meta"},[n("a",{staticClass:"no-marker linkedin",attrs:{target:"_blank",href:e.socialProfiles.linkedIn,"aria-label":"Connect with Olaolu on LinkedIn, opens a new tab."},on:{click:function(t){return e.$ga.event("Resume","click","LinkedIn Button")}}},[n("LinkedInIcon")],1),n("a",{staticClass:"no-marker",attrs:{href:"mailto:hello@olaolu.dev","aria-label":"Send Olaolu a mail"},on:{click:function(t){return e.$ga.event("Resume","click","Mail Button")}}},[n("MailIcon")],1),n("a",{staticClass:"no-marker",attrs:{href:e.resumePDF,target:"_blank",hidden:e.isPDF,"aria-label":"Download a PDF copy of this resume, opens a new tab."},on:{click:function(t){return e.$ga.event("Resume","click","Download PDF Button")}}},[n("DownloadIcon"),e._v("\n      Download\n    ")],1)]),n("div",{attrs:{id:"resume-outlines"}},[n("section",[n("a",{attrs:{href:"https://olaolu.dev","aria-label":"olaolu dot dev"}},[e._v("olaolu.dev")]),n("span",[e._v("Lagos, Nigeria")]),n("a",{attrs:{href:"mailto:hello@olaolu.dev"},on:{click:function(t){return e.$ga.event("Resume","click","Mail Link in Sidebar")}}},[e._v("\n        hello@olaolu.dev\n      ")])]),n("section",[n("h3",[e._v("Core Technologies:")]),n("ul",e._l(e.data.technologies,(function(t,i){return n("li",{key:i},[e._v(e._s(t))])})),0)]),n("section",[n("h3",[e._v("Others:")]),n("ul",e._l(e.data.otherProficiencies,(function(t,i){return n("li",{key:i},[e._v(e._s(t))])})),0)])]),n("article",[n("header",{attrs:{id:"profile-summary"}},[n("h1",{attrs:{id:"name"}},[e._v("Olaolu "),n("br"),e._v("Olawuyi")]),n("h2",[e._v("Expert Frontend developer and UX Engineer.")]),n("p",[e._v("\n        Engineer valued for driving high-performance accessible web\n        experiences. I design quality, user-friendly and scalable products\n        regardless of stack.\n      ")])]),n("section",{attrs:{id:"experience"}},[n("h3",{staticClass:"heading"},[e._v("Experience")]),n("p",[e._v("\n        I’ve worked on a handful of web projects over the past\n        "+e._s(e.YOE)+" years, some of which were for the following organizations:\n      ")]),n("ul",{attrs:{id:"companies"}},e._l(e.data.companies,(function(t,i){return n("li",{key:i,staticClass:"company"},[n("header",[n("h4",[e._v(e._s(t.name)+" "),n("span",[e._v(e._s(t.role))])]),n("span",{staticClass:"period"},[e._v(e._s(t.period))])]),n("p",[e._v(e._s(t.intro))]),n("ul",{staticClass:"points"},e._l(t.points,(function(t,i){return n("li",{key:i},[e._v("\n              "+e._s(t)+"\n            ")])})),0),n("p",{directives:[{name:"g-image",rawName:"v-g-image"}],staticClass:"outro",domProps:{innerHTML:e._s(t.outro)}})])})),0)]),n("section",{attrs:{id:"projects"}},[n("h3",{staticClass:"heading"},[e._v("Projects")]),n("p",[e._v("\n        Links to some of my work can be found on\n        "),n("a",{attrs:{href:"https://olaolu.dev/work"},on:{click:function(t){return e.$ga.event("Resume","click","Work link",{transport:"beacon"})}}},[e._v("\n          olaolu.dev/work\n        ")]),e._v("\n        and details can be provided upon request via a scheduled demo call.\n      ")])]),n("SauceDripLogo",{attrs:{id:"logo"}})],1)])}),[],!1,null,null,null);t.default=g.exports}}]);
